18220cd647537e70445a8a70f14e65e14fef29b4
suraj0522 <surajk256666@gmail.com>
2025-06-28 19:42:06 +0530

    add a string different type od varibal

--- string.js
+++ string.js
@@ -1,2 +1,44 @@
-let str="you are a gun";
-let str2="yo yo yo";
+let str = "you are a gun";
+let str2 = "yo yo yo";
+console.log(str[0]);
+console.log(str[1]);
+console.log(str[2]);
+console.log(str[3]);
+console.log(str[4]);
+console.log(str[5]);
+console.log(str[6]);
+console.log(str[7]);
+console.log(str[8]);
+console.log(str[9]);
+console.log(str[10]);
+console.log(str[11]);
+console.log(str[12]);
+
+let sepstr = `its a special string`;
+console.log(sepstr);
+
+let obj = {
+  name: "pen",
+  price: 10,
+  color: "red",
+};
+
+let output = `the name of the object is ${obj.name} and the price is ${obj.price} and the color is ${obj.color}`;
+console.log(output);
+
+let strrr = "Hello there ";
+console.log(strrr.slice(0, 6));
+
+let result = strrr.concat(str);
+console.log(result);
+
+let ab = "jello";
+console.log(ab.replace("j", "p"));
+
+let chhar= "suraj";
+ console.log(chhar.charAt(0));
+
+ let fullname = prompt("Enter your full name without spaces");
+ 
+ let username = "@"+ fullname + fullname.length;
+ console.log(username);
