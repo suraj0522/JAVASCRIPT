553011432c2d3554eadf96e2cc151b29d83fe305
suraj0522 <surajk256666@gmail.com>
2025-06-30 19:36:26 +0530

    make some loop

--- array.js
+++ array.js
@@ -3,5 +3,16 @@ console.log(marks);
 console.log(marks[0]);
 console.log(marks[1]=85);
 
-let names = ["suraj","subhan", "ayush"];
+let names = ["suraj","subhan", "amnjubg"];
+
+for(let idx =0; idx<names.length; idx++){
+    console.log(names[idx]);
+}
+
 console.log(names);
+
+let cities = ["delhi","mumbai","pune","ranchi"];
+
+for(let city of cities){
+     console.log(city.toUpperCase());
+}
