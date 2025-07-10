42beb188b9fbb39d5f10dab557fca92a5d53fb42
suraj0522 <surajk256666@gmail.com>
2025-07-10 19:26:19 +0530

    new type of function

---
+++ function.js
@@ -0,0 +1,19 @@
+function myfun(){
+    console.log("Hello there");
+    console.log("This is my first function");
+}
+myfun();
+myfun();
+
+function letter(msg){
+    console.log(msg );
+}
+
+letter("This is a message from the letter function");
+
+function add(a, b){
+   s = a + b;
+   return s;
+}
+ let sum =add(5, 10);
+console.log("The sum is:",sum);
