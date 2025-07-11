dc6097311760d08a9d25852f65a68a4e0a1afe38
suraj0522 <surajk256666@gmail.com>
2025-07-11 20:17:13 +0530

    add sums

--- function.js
+++ function.js
@@ -1,19 +1,55 @@
-function myfun(){
-    console.log("Hello there");
-    console.log("This is my first function");
+function myfun() {
+  console.log("Hello there");
+  console.log("This is my first function");
 }
 myfun();
 myfun();
 
-function letter(msg){
-    console.log(msg );
+function letter(msg) {
+  console.log(msg);
 }
 
 letter("This is a message from the letter function");
 
-function add(a, b){
-   s = a + b;
-   return s;
+function add(a, b) {
+  s = a + b;
+  return s;
 }
- let sum =add(5, 10);
-console.log("The sum is:",sum);
+let sum = add(5, 10);
+console.log("The sum is:", sum);
+
+const arrowsum = (a, b) => {
+  console.log(a + b);
+};
+
+function multiple(a, b) {
+  return a * b;
+}
+arrowsum(5, 10);
+
+const arrowmultiple = (a, b) => {
+  console.log(a * b);
+};
+
+const printHello = () => {
+  console.log("Hello");
+};
+printHello();
+
+function countvowels(str) {
+  let count = 0;
+  for (const char of str) {
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+
+  console.log(count);
+}
+countvowels("suraj");
