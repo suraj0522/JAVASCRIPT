bcea8d54fb390254ed2a99b23b296880dd6d5226
suraj0522 <surajk256666@gmail.com>
2025-08-01 19:39:05 +0530

    make a div using dom

--- DOM.js
+++ DOM.js
@@ -23,3 +23,19 @@ console.dir(secondel);
 console.dir(document.body.firstChild);
 
 console.log(document.querySelector("div").children);
+
+let div = document.querySelector("div");
+console.dir(div);
+
+let h2 = document.querySelector("h2");
+h2.innerText = h2.innerText + " is changed now";
+console.dir(h2.innerText);
+
+let box = document.querySelectorAll(".box");
+ console.dir(box);
+let idx =1;
+ for(div of box){
+     div.innerText = `this is div ${idx}`;
+     idx++;
+     
+} //this is a loop system of js 
