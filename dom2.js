e3f5aca5bd65937ad8bf9527da1133b419c0f125
suraj0522 <surajk256666@gmail.com>
2025-08-08 19:31:23 +0530

    adding buton using js

--- dom2.js
+++ dom2.js
@@ -1,6 +1,18 @@
-let div = document.querySelector("div");
+// let div = document.querySelector("div");
 
-console.log(div);
+// console.log(div);
+
+// div.style.backgroundColor = "purple";
+
+let newbtn = document.createElement("button");
+newbtn.innerText = "Click Me!";
+console.log(newbtn);
+
+let div = document.querySelector("div");
+div.prepend(newbtn);
+
+
+
 
 
 
